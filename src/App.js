8e22c6ab7baacc01a43185054bc987421234412e
David Jung <david.jung@taggle.co>
2017-07-07 15:06:07 +0900

    MaterialUI 적용 및 SideNav(Drawer) 적용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import MenuItem from 'material-ui/MenuItem';
+import Drawer from 'material-ui/Drawer';
 import './App.css';
 
+const LeftNav = (props) => {
+  return <Drawer
+    open={props.open}>
+    <MenuItem onClick={props.handleClose}>Menu Item</MenuItem>
+    <MenuItem onClick={props.handleClose}>Menu Item 2</MenuItem>
+  </Drawer>
+}
+
 class App extends Component {
-  render() {
+  state = { open: false }
+
+  handleToggle = () => this.setState({ open: !this.state.open })
+  handleClose = () => this.setState({ open: false })
+
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <RaisedButton label="Open Drawer" onClick={this.handleToggle}/>
+          <LeftNav open={this.state.open} handleClose={this.handleClose} />
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h2>Welcome to React</h2>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
